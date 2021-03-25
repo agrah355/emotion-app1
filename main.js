117b7a1a3e14189aac8b10842745fc24e61f4bbe
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-03-25 15:40:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 Webcam.set({
-    width: 320,
-    height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
-  });
-  Webcam.attach("#camera");
+    width: 350,
+    height: 300,
+    image_format:"png",
+    png_quality: 90
+    }
+    );
+    camera=document.getElementById("camera");
+    
+    Webcam.attach("#camera");
 
   function takeSnapshot(){
       Webcam.snap(function(data_uri){
           document.getElementById("result").innerhtml="<img id='result1' src='"+data_uri+"'>";
       });
   }
-  classifier=Image.classifier("https://teachablemachine.withgoogle.com/models/rI8XG-cre/modlel.json",modleLodded);
+  console.log("ml5 version : ",ml5.version);
+  classifier=Image.classifier("https://teachablemachine.withgoogle.com/models/Qct9MqG8S/model.json",modleLodded);
   function modleLodded(){
-      console.log("ml5 version",ml5.version);
       console.log("modle lodded true");
+  }
+  function check(){
+      image=document.getElementById("result1");
+      classifier.classify(image, gotresult);
+  }
+  function gotresult(error,results){
+      if(error){
+          console.erorr(error);
+      }else{
+          console.log(results);
+          document.getElementById("name_label").innerHTML=results[0].label;
+          document.getElementById("accuracy_label").innerHTML=results[0].confidence.toFixed(3);
+      }
   }
